426df3bb9801cf3d4c888fa2309d8cfcbfa0da06
Celia71-z <987399445@qq.com>
2026-07-23 11:11:02 +0800

    Use travel-themed logo icon

--- src/modules/home/ui/components/header/logo.tsx
+++ src/modules/home/ui/components/header/logo.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import WordRotate from "../word-rotate";
-import { RiCameraLensFill } from "react-icons/ri";
+import { Compass } from "lucide-react";
 import { siteConfig } from "@/site.config";
 
 const Logo = () => {
   return (
     <Link href="/" className="flex gap-2 items-center">
-      <RiCameraLensFill size={18} />
+      <Compass size={18} />
       <WordRotate
         label={siteConfig.name}
         label2={siteConfig.tagline}
